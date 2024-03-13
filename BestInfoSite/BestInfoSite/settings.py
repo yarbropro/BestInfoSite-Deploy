ec680e3785bd2deacdee5b625f682ca4a7ce91fb
yarbropro <samchukyaroslavofficial@gmail.com>
2024-03-13 10:12:32 +0000

    13.0

--- BestInfoSite/BestInfoSite/settings.py
+++ BestInfoSite/BestInfoSite/settings.py
@@ -129,6 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'Base')
+print(STATIC_ROOT)
+print(STATIC_URL)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
